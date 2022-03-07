717e8bc9052fbbca489e39b284c875239853710f
Felipe Iregui <pireguis_Ca64@hotmail.com>
2022-03-07 13:01:52 +0100

    avances

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,120 @@
-import { useEffect } from "react";
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getContacts, postContact, deleteContact } from '../services/contact';
+import Swal from 'sweetalert2';
 
 const App = () => {
-    const url = 'http://localhost:3000/contactos';
-    fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data));
+    const [contacts, setContacts] = useState([]);
+
+    useEffect(() => {
+        getContacts().then((response) => {
+            setContacts(response);
+        })
+    }, []);
+
+    const getHistory = (cambios) => {
+        let template = ``;
+        cambios.forEach(cambio => {
+            console.log(cambio.fecha);
+            template += `<p>${cambio.cambio} en ${new Date(cambio.fecha).toLocaleString()}</p>`;
+        })
+
+        Swal.fire({
+            title: "Edits",
+            html: template,
+            confirmButtonText: 'Exit'
+        })
+    }
+
+    const postContacts = () => {
+        Swal.fire({
+            title: 'Submit the data you want to create',
+            html: '<form><input type="text" id="Name" class="swal2-input" placeholder="Name"><input type="text" id="lastname" class="swal2-input" placeholder="LastName"><input type="email" id="email" class="swal2-input" placeholder="Email"><input type="number" id="phone" class="swal2-input" placeholder="Phone Number"></form>',
+            inputAttributes: {
+                autocapitalize: 'off'
+            },
+            showCancelButton: true,
+            confirmButtonText: 'Create',
+            showLoaderOnConfirm: true,
+            preConfirm: () => {
+                const name = Swal.getPopup().querySelector('#Name').value;
+                const lastname = Swal.getPopup().querySelector('#lastname').value;
+                const email = Swal.getPopup().querySelector('#email').value;
+                const phone = Swal.getPopup().querySelector('#phone').value;
+                console.log(typeof phone, parseInt(phone));
+                if (isNaN(parseInt(phone))) {
+                    return Swal.showValidationMessage(
+                        `Request failed: Phone must be a number`
+                    )
+                }
+                return postContact(name, lastname, email, phone)
+                    .then(response => {
+                        console.log("res" + response);
+                        setContacts(contacts.push({   
+                            "nombre": name,
+                            "apellido": lastname, 
+                            "email": email,
+                            "celular": phone
+                        }))
+                        return response
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        Swal.showValidationMessage(
+                            `Request failed: Repeated Email `
+                        )
+                    })
+            },
+            allowOutsideClick: () => !Swal.isLoading()
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // setContacts(contacts.push(result));
+                Swal.fire({
+                    title: `Contact created properly with email ${result.value.email}`
+                })
+            }
+        })
+    }
+
+    const deleteContactA = (email, index) => {
+        deleteContact(email).then(response => {
+            contacts.splice(index, 1);
+            setContacts(contacts);
+        });
+    }
+
     return (
-        <h1>React</h1>
+        <React.Fragment>
+            <h1>Contacts!</h1>
+            <input></input>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Phone Number</th>
+                        <th>Edits</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {contacts.map((item, index) => {
+                        return (
+                            <tr key={item.email}>
+                                <td> {item.nombre} </td>
+                                <td> {item.apellido} </td>
+                                <td> {item.email} </td>
+                                <td> {item.celular} </td>
+                                <td> <button type="button" className="btn btn-primary"
+                                    onClick={() => getHistory(item.cambios)}>Show Edits </button></td>
+                                <td> <button type="button" className="btn btn-danger"
+                                    onClick={() => deleteContactA(item.email, index)}>Delete </button></td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            <button type="button" className="btn btn-primary" onClick={postContacts}>Create New</button>
+        </React.Fragment>
     )
 }
 
---
+++ src/services/contact.js
@@ -0,0 +1,51 @@
+const url = 'http://localhost:3000/contactos';
+
+export function getContacts() {
+    return fetch(url).then((response) => {
+        return response.json();
+    });
+}
+
+export function postContact(name, lastname, email, phone) {
+    return new Promise((resolve, reject) => fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ nombre: name, apellido: lastname, email: email, celular: phone })
+    }).then((response) => {
+        console.log(response);
+        return resolve(response.json());
+    })
+        .catch(error => {
+            console.log(error);
+            return reject(error)
+        })
+    )
+}
+
+export function deleteContact(email) {
+    return new Promise((resolve, reject) => fetch(url+`/${email}`, {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+    }).then((response) => {
+        console.log(response);
+        return resolve(response.json());
+    })
+        .catch(error => {
+            console.log(error);
+            return reject(error)
+        })
+    )
+}
+
+
+// export function getKeyContact() {
+//     return getContacts().then(response => {
+//         response.
+//     })
+// }
